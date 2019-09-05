07eaa1b188cb38f45b3bfeaf6fcd5dffa84e44ed
andrewhall <hallal17@wfu.edu>
2019-09-05 15:45:04 -0400

    Part 1

--- main.cpp
+++ main.cpp
@@ -5,23 +5,27 @@ int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
     int revVctr[8];               // User values
     int i;                        // Loop index
+    int temp;
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
+    for (i = 0; i < NUM_ELEMENTS; i++) {
         cin >> revVctr[i];
     }
 
-    // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+    // Reverses through the original array and will store val in temp in order to swap
+    for (i = 0; i < (NUM_ELEMENTS/2); i++) {
+        temp = revVctr[i];
+        revVctr[i] = revVctr[(NUM_ELEMENTS - 1) -i]; // Swap
+        revVctr[(NUM_ELEMENTS - 1) -i] = temp;
     }
 
     // Print values
     cout << endl << "New values: ";
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
+    for (i = 0; i < NUM_ELEMENTS; i++) {
         cout << " " << revVctr[i];
     }
     cout << endl;
 
     return 0;
 }
+
